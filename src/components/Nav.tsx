d127a160bf42717929db9fdc8335ef8a30cc26f0
wanxiankai <wanxiankai@126.com>
2020-08-15 16:28:37 +0800

    completed this icon cmp

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
 //border:1px blue solid;
@@ -11,7 +14,14 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
       >li {
       text-align:center;
       width:33.3333%;
-      padding:16px
+      display:flex;
+      padding: 4px 0;
+      flex-direction: column;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,12 +31,21 @@ const Nav = ()=> {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
