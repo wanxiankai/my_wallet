1192aae0f25daa8d4f7a0b2fff3aa63e527d4c38
wanxiankai <wanxiankai@126.com>
2020-08-18 21:56:14 +0800

    TagsSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,35 @@ import Tags from './views/Tags';
 import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import NoMatch from 'views/NoMatch';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/tags">
-            <Tags/>
-          </Route>
-          <Route path="/money">
-            <Money/>
-          </Route>
-          <Route path="/statistics">
-            <Statistics/>
-          </Route>
-          <Redirect exact from="/" to="/money"/>
-          <Route path="*">
-            <NoMatch/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AppWrapper>
   );
 }
 
